53180a8f60ac252a81ef606233cb97382aabd1b3
hackee12 <hackee.owl@gmail.com>
2024-03-28 02:08:49 -0400

    cleanup

--- src/test/java/org/example/comparator/readwritedemo/WriteGenericRecordToAvroJsonDemoTest.java
+++ src/test/java/org/example/comparator/readwritedemo/WriteGenericRecordToAvroJsonDemoTest.java
@@ -2,43 +2,53 @@ package org.example.comparator.readwritedemo;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.io.JsonDecoder;
 import org.apache.avro.io.JsonEncoder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 class WriteGenericRecordToAvroJsonDemoTest {
 
     static final String SCHEMA_FILE = "src/test/resources/user.avsc";
-    static final String USERS_AVRO_JSON = "src/test/resources/users.avro.json";
 
     @Disabled
     @Test
-    void writeGenericRecordToAvroJsonDemo() throws IOException{
+    void writeToAvroJsonThenReadFromAvroJson() throws IOException {
         Schema schema = new Schema.Parser().parse(new File(SCHEMA_FILE));
 
-        GenericData.Record user1 = new GenericRecordBuilder(schema)
+        GenericData.Record userWrite = new GenericRecordBuilder(schema)
+                .set("id", "123")
                 .set("name", "User1")
                 .set("favorite_number", 42)
                 .set("favorite_color", "red")
                 .build();
 
-        try (
-                OutputStream jsonOut = Files.newOutputStream(Paths.get(USERS_AVRO_JSON))
-        ) {
+        try (var byteOut = new ByteArrayOutputStream()) {
             GenericDatumWriter<Object> writer = new GenericDatumWriter<>(schema);
-            JsonEncoder jsonEncoder = EncoderFactory.get().jsonEncoder(schema, jsonOut);
+            JsonEncoder jsonEncoder = EncoderFactory.get().jsonEncoder(schema, byteOut);
 
-            writer.write(user1, jsonEncoder);
+            writer.write(userWrite, jsonEncoder);
             jsonEncoder.flush();
+
+            try (var byteIn = new ByteArrayInputStream(byteOut.toByteArray())) {
+                GenericDatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
+                JsonDecoder decoder = DecoderFactory.get().jsonDecoder(schema, byteIn);
+
+                GenericRecord userRead = reader.read(null, decoder);
+                Assertions.assertEquals(userWrite, userRead);
+            }
         }
     }
 }
