f0759aa8637fb6c9f5de5788d22556097326b86f
hackee12 <hackee.owl@gmail.com>
2024-03-28 00:32:01 -0400

    cleanup

--- src/main/java/org/example/comparator/comparators/naive/NaiveComparator.java
+++ src/main/java/org/example/comparator/comparators/naive/NaiveComparator.java
@@ -11,7 +11,6 @@ import org.example.comparator.comparators.AvroComparator;
 import org.example.comparator.comparators.AvroKeyResolver;
 import org.example.comparator.domain.DeepDiff;
 import org.example.comparator.domain.DiffEngineConfig;
-import org.example.comparator.domain.DiffLayout;
 import org.example.comparator.domain.ThinAvroDiff;
 import org.example.comparator.exception.AvroComparatorException;
 import org.example.comparator.util.SchemaUtil;
@@ -29,7 +28,9 @@ import static java.util.Objects.isNull;
 @Getter
 public class NaiveComparator implements AvroComparator<DeepDiff> {
 
-    private static final DiffLayout LAYOUT = NaiveLayout.INSTANCE;
+    private static final String TEMPLATE_RECORD_DELIMITER = "%s.";
+    private static final String TEMPLATE_RECORD_FIELD_DELIMITER = "%s%s.";
+
     private static final String TYPE_NOT_EXPECTED = "This is a bug! I did not expect %s type in here.";
     private static final String TYPE_NOT_SUPPORTED_IN_DEMO = "%s type is not supported in this demo version.";
 
@@ -37,53 +38,53 @@ public class NaiveComparator implements AvroComparator<DeepDiff> {
     private DiffEngineConfig diffEngineConfig;
 
     @Override
-    public List<ThinAvroDiff> getDiff(Schema reader, GenericRecord left, GenericRecord right) {
+    public List<ThinAvroDiff> getDiff(Schema recordSchema, GenericRecord left, GenericRecord right) {
         if (left == right) {
             throw new AvroComparatorException(new IllegalArgumentException(
                     "This comparator does not support comparing objects to themselves."
             ));
         }
-        return traverseRecordGenericRecords(reader.getName(), reader, left, right);
+        final String nodeLevel = String.format(TEMPLATE_RECORD_DELIMITER, recordSchema.getName());
+        return traverseRecordFields(nodeLevel, recordSchema, left, right);
     }
 
-    private List<ThinAvroDiff> traverseRecordGenericRecords(String parent, Schema recordSchema, GenericRecord left, GenericRecord right) {
+    private List<ThinAvroDiff> traverseRecordFields(
+            String nodeLevel, Schema recordSchema, GenericRecord left, GenericRecord right
+    ) {
         final List<ThinAvroDiff> diff = new ArrayList<>();
-        final String level = parent + LAYOUT.getDelimiter();
-
         for (Schema.Field field : recordSchema.getFields()) {
-            final Schema fieldDatumSchema = SchemaUtil.getDatumSchema(field.schema());
-            final Schema.Type fieldDatumType = fieldDatumSchema.getType();
             final Object fieldValueLeft = left == null ? null : left.get(field.name());
             final Object fieldValueRight = right == null ? null : right.get(field.name());
-
             if (isNull(fieldValueLeft) && isNull(fieldValueRight)) {
                 continue;
             }
-
+            final Schema.Type fieldDatumType = SchemaUtil.getDatumSchema(field.schema()).getType();
             switch (fieldDatumType) {
-
                 case NULL, UNION -> throw new AvroRuntimeException(new RuntimeException(
                         format(TYPE_NOT_EXPECTED, fieldDatumType)));
-
                 case FIXED, BYTES, ENUM, MAP, ARRAY -> throw new AvroRuntimeException(new RuntimeException(
                         format(TYPE_NOT_SUPPORTED_IN_DEMO, fieldDatumType)));
-
-                case RECORD ->
-                        diff.addAll(traverseRecordGenericRecords(level + field.name(), fieldDatumSchema, (GenericRecord) fieldValueLeft, (GenericRecord) fieldValueRight));
-
+                case RECORD -> diff.addAll(getRecordDiff(
+                        nodeLevel, field, (GenericRecord) fieldValueLeft, (GenericRecord) fieldValueRight));
                 case STRING, INT, LONG, FLOAT, DOUBLE, BOOLEAN ->
-                        diff.addAll(traverseLeaf(level, field, fieldValueLeft, fieldValueRight));
+                        diff.addAll(getLeafDiff(nodeLevel, field, fieldValueLeft, fieldValueRight));
             }
         }
         return diff;
     }
 
-    private List<ThinAvroDiff> traverseLeaf(String parent, Schema.Field leaf, Object left, Object right) {
+    private List<ThinAvroDiff> getRecordDiff(
+            String parent, Schema.Field recordField, GenericRecord left, GenericRecord right
+    ) {
+        final String nodeLevel = String.format(TEMPLATE_RECORD_FIELD_DELIMITER, parent, recordField.name());
+        final Schema recordSchema = SchemaUtil.getDatumSchema(recordField.schema());
+        return traverseRecordFields(nodeLevel, recordSchema, left, right);
+    }
 
+    private List<ThinAvroDiff> getLeafDiff(String parent, Schema.Field leaf, Object left, Object right) {
         if (Objects.equals(left, right)) {
             return List.of();
         }
-
         final String leafLevel = parent + leaf.name();
         final String leafType = SchemaUtil.getDatumSchema(leaf.schema()).getType().getName();
         return List.of(
